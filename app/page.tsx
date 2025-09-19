2dd1ef859ae72289a82667ce3439e2edf43c3a52
danyalahmad2000 <danigurrero.dg@gmail.com>
2025-09-19 14:02:18 +0500

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1165,7 +1165,7 @@ export default function Home() {
                 <Button
                   size="lg"
                   variant="outline"
-                  className="border-white text-white hover:bg-white hover:text-[#1A365D]"
+                  className="border-white hover:bg-white text-[#1A365D]"
                 >
                   <Award className="w-5 h-5 mr-2" />
                   View Portfolio
